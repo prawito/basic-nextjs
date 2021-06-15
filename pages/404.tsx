401d2699a8f72bda4709745aae4248ed39b570fc
Prawito Hudoro <prawitohudoro@gmail.com>
2021-06-16 02:48:18 +0700

    custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Ooops....</h1>
+      <h1 className="title-not-found">Halam yang anda cari tidak ditemukan</h1>
+    </div>
+  );
+}
