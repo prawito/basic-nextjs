a5f326d37ebf07b870670f2d6de66ae6d0971fa2
Prawito Hudoro <prawitohudoro@gmail.com>
2021-06-16 06:41:10 +0700

    data fetching getServerSideProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,34 @@
 import Layout from '../components/Layout';
+import styles from '../styles/Blog.module.css';
 
-export default function Blog() {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+interface BlogProps {
+  dataBlog: Post[]
+}
+export default function Blog(props: BlogProps) {
+  const { dataBlog } = props;
   return (
     <Layout pageTitle="Blog Page">
-      <p className="title-homepage">Blog Page</p>
+      {dataBlog.map((blog) => (
+        <div key={blog.id} className={styles.card}>
+          <h3>{blog.title}</h3>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const dataBlog = await res.json();
+  return {
+    props: {
+      dataBlog,
+    },
+  };
 }
