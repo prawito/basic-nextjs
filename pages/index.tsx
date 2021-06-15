b3f14ab6a292639ccc6e5e8de9cb774f97d066ae
Prawito Hudoro <prawitohudoro@gmail.com>
2021-06-16 03:48:19 +0700

    optimalisasi image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,63 @@
-import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout pageTitle="Home Page">
+      <Image src="/profile.png" width={200} height={200} alt="profile" />
       <h1 className={styles['title-homepage']}>Welcome Prawito</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <Image src="/post1.png" width={200} height={200} alt="post image 1" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores accusamus ullam aperiam dicta expedita similique cum totam nemo sunt quos porro nesciunt adipisci, laudantium iste eos sed illum. Veniam, explicabo.</p>
+      <Image src="/post2.png" width={200} height={200} alt="post image 2" />
     </Layout>
   );
 }
