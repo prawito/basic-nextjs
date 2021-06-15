7c3c2ce646bea52be4aa941ededb11d89283a2c4
Prawito Hudoro <prawitohudoro@gmail.com>
2021-06-16 03:09:45 +0700

    auto redirection

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 2000);
+  }, []);
   return (
     <div>
       <h1 className="title-not-found">Ooops....</h1>
